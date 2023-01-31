971c4b872aaef75adb3e4448e8b195203180cf11
KuldeepChotrani <kuldeep.chotrani@ignitesol.com>
2023-01-31 11:39:54 +0530

    text fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-JFCustomWidget.subscribe("ready", function() {
+JFCustomWidget.subscribe("ready", function(){
     var phSlider = document.getElementById("phSlider");
     var sliderLength = JFCustomWidget.getWidgetSetting('Maximum');
     var valuesForSlider = Array.from({ length: sliderLength }, (_, i) => i + 1);
-
+    
     var format = {
         to: function (value) {
             return valuesForSlider[Math.round(value)];
@@ -11,7 +11,7 @@ JFCustomWidget.subscribe("ready", function() {
             return valuesForSlider.indexOf(Number(value));
         }
     };
-
+    
     noUiSlider.create(phSlider, {
         start: 5,
         range: { min: 0, max: valuesForSlider.length - 1 },
@@ -20,33 +20,28 @@ JFCustomWidget.subscribe("ready", function() {
         pips: { mode: "steps", density: 3, format },
         format
     });
-
+    
     // The display values can be used to control the slider
     phSlider.noUiSlider.set(1);
-
-    var sliderContext = document.getElementById("sliderContext");
-    sliderContext.firstElementChild.textContent = sliderContextMessages.left;
-    sliderContext.querySelector('.center').textContent = sliderContextMessages.center;
-    sliderContext.lastElementChild.textContent = sliderContextMessages.right;
-
-    var label = JFCustomWidget.getWidgetSetting('QuestionLabel');
-    document.getElementById('labelText').innerHTML = label;
-    var sliderContext = document.getElementById("sliderContext");
-    var firstText = JFCustomWidget.getWidgetSetting('firstText');
-    sliderContext.firstElementChild.textContent = firstText
-    var secondText = JFCustomWidget.getWidgetSetting('secondText');
-    sliderContext.querySelector('.center').textContent = secondText
-    var thirdText = JFCustomWidget.getWidgetSetting('thirdText');
-    sliderContext.lastElementChild.textContent = thirdText
-    //subscribe to form submit event
-    JFCustomWidget.subscribe("submit", function() {
-        var msg = {
-            //you should valid attribute to data for JotForm
-            //to be able to use youw widget as required
-            valid: true,
-            value: phSlider.noUiSlider.get()
-        }
-        // send value to JotForm
-        JFCustomWidget.sendSubmit(msg);
+    
+        var label = JFCustomWidget.getWidgetSetting('QuestionLabel');
+        document.getElementById('labelText').innerHTML = label;
+        var sliderContext = document.getElementById("sliderContext");
+        var firstText = JFCustomWidget.getWidgetSetting('firstText');
+        sliderContext.firstElementChild.textContent = firstText
+        var secondText = JFCustomWidget.getWidgetSetting('secondText');
+        sliderContext.querySelector('.center').textContent = secondText
+        var thirdText = JFCustomWidget.getWidgetSetting('thirdText');
+        sliderContext.lastElementChild.textContent = thirdText
+        //subscribe to form submit event
+        JFCustomWidget.subscribe("submit", function(){
+            var msg = {
+                //you should valid attribute to data for JotForm
+                //to be able to use youw widget as required
+                valid: true,
+                value: phSlider.noUiSlider.get()
+            }
+            // send value to JotForm
+            JFCustomWidget.sendSubmit(msg);
+        });
     });
-});
